b186ba68705d3c422bba10cef69936e7008f8ac3
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2020-11-08 23:48:27 +0530

    -Added Widgets like MaterialApp , Scaffolds, AppBar, Text, Image, Color

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 //The main app is the starting point to all our Flutter Projects
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello world'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'),
+        ),
+      ),
     ),
   ));
 }
