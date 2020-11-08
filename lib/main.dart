b79ad5d1853a3dc8b0ce7c2f1986b7562dcd3045
Dhanraj Naik <dhanraj.naik@bidchat.com>
2020-11-09 00:06:58 +0530

    -added local asset image and loaded in Image Widget -pubspec has 2 spaces for each indent file

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ void main() {
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Image(
-          image: NetworkImage(
-              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'),
-        ),
+            image:
+                // NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'),
+                AssetImage('images/diamond.png')),
       ),
     ),
   ));
